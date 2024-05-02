8ed84568b2d6a29fbc61730f61e3649e76761236
minyekim <biie2004@naver.com>
2024-05-02 21:44:31 +0900

    Add : 형의 main함수들, 테스트용

---
+++ main/print_test.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_test.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 20:30:34 by minyekim          #+#    #+#             */
+/*   Updated: 2024/05/02 20:30:40 by minyekim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	display_list(t_token_list **head)
+{
+	t_token_list	*now;
+
+	now = (*head);
+	while (now != NULL)
+	{
+		printf("TOKEN>>>[%s]", now->token);
+		type_print(now);
+		now = now->next;
+	}
+}
+
+void	type_print(t_token_list *now)
+{
+	int	type;
+
+	type = now->type;
+	if (type == NONE)
+		printf(" : TYPE[NONE]\n");
+	else if (type == CMD)
+		printf(" : TYPE[CMD]\n");
+	else if (type == ARG)
+		printf(" : TYPE[ARG]\n");
+	else if (type == PIPE)
+		printf(" : TYPE[PIPE]\n");
+	else if (type == IN_D1)
+		printf(" : TYPE[IN_D1]\n");
+	else if (type == IN_D2)
+		printf(" : TYPE[IN_D2]\n");
+	else if (type == OUT_D1)
+		printf(" : TYPE[OUT_D1]\n");
+	else if (type == OUT_D2)
+		printf(" : TYPE[OUT_D2]\n");
+	else if (type == IN_D1_ARG)
+		printf(" : TYPE[IN_D1_ARG]\n");
+	else if (type == IN_D2_ARG)
+		printf(" : TYPE[IN_D2_ARG]\n");
+	else if (type == OUT_D1_ARG)
+		printf(" : TYPE[OUT_D1_ARG]\n");
+	else if (type == OUT_D2_ARG)
+		printf(" : TYPE[OUT_D2_ARG]\n");
+	else if (type == IN_OPEN_D1)
+		printf(" : TYPE[IN_OPEN_D1]\n");
+	else if (type == IN_OPEN_D2)
+		printf(" : TYPE[IN_OPEN_D2]\n");
+	else if (type == OUT_OPEN_D1)
+		printf(" : TYPE[OUT_OPEN_D1]\n");
+	else if (type == OUT_OPEN_D2)
+		printf(" : TYPE[OUT_OPEN_D2]\n");
+	else if (type == WRONG_TYPE)
+		printf(" : TYPE[WRONG_TYPE]\n");
+	else if (type == END)
+		printf(" : TYPE[END]\n");
+}
+
+void	list_free(t_token_list **head)
+{
+	t_token_list	*temp;
+	t_token_list	*remove;
+
+	temp = *head;
+	if (temp == NULL)
+		return ;
+	while (temp->next != NULL)
+	{
+		remove = temp;
+		temp = temp->next;
+		free(remove);
+	}
+	return ;
+}
