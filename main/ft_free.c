20260c4c6ef9c0ec3109ff97619c2f754ccb0e4c
minyekim <biie2004@naver.com>
2024-05-07 21:28:43 +0900

    Add : minishell_utils.c 파일에 있던 t_token_list_free, array_2d_free 함수를 옮김

---
+++ main/ft_free.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 17:40:38 by minyekim          #+#    #+#             */
+/*   Updated: 2024/05/07 19:26:08 by minyekim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	t_token_list_free(t_token_list **head)
+{
+	t_token_list	*tmp;
+
+	while ((*head) != NULL)
+	{
+		tmp = (*head)->next;
+		free((*head)->token);
+		(*head)->token = NULL;
+		free((*head));
+		(*head) = NULL;
+		(*head) = tmp;
+	}
+}
+
+void	array_2d_free(void **arr)
+{
+	int	i;
+	
+	i = 0;
+	if (arr == NULL)
+		return ;
+	while (arr[i] != NULL)
+	{
+		free(arr[i]);
+		arr[i] = NULL;
+		i++;
+	}
+	free(arr);
+}
