51154e25cad970e2437b2c5bfb8a61671fb8824f
minyekim <biie2004@naver.com>
2024-05-07 21:31:31 +0900

    Add : here_doc.c에 있던 here_doc_file_unlink, ft_limiter_check함수를 옮김 ft_limiter_check함수에서 limiter가 정상 작동하지 않던 부분 해결 ex) limiter == asdf, line asdfg일 경우 정상 종료되는 부분을 해결

---
+++ main/here_doc_utils.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 17:49:52 by minyekim          #+#    #+#             */
+/*   Updated: 2024/05/07 20:56:40 by minyekim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	here_doc_file_unlink(int here_doc_cnt)
+{
+	int		cnt;
+	char	*filename;
+	char	*tmp;
+	
+	cnt = 1;
+	while (cnt <= here_doc_cnt)
+	{
+		tmp = ft_itoa(cnt);
+		filename = ft_strjoin(".temp", tmp);
+		unlink(filename);
+		free(tmp);
+		free(filename);
+		cnt++;
+	}
+}
+
+int	ft_limiter_check(char *str1, char *str2)
+{
+	size_t	i;
+
+	i = 0;
+	while (str1[i] != '\0' && str2[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			return (NO_LIMITER);
+		i++;
+	}
+	if (str1[i] == '\n' && str1[i + 1] == '\0' && str2[i] == '\0')
+		return (YES_LIMITER);
+	return (NO_LIMITER);
+}
