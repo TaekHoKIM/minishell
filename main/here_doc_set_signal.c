40f32991863cc13dfcec058d2e91a9101056dd1b
minyekim <biie2004@naver.com>
2024-05-07 21:29:56 +0900

    Add : here_doc 상태에서 시그널 입력시 실행되는 함수

---
+++ main/here_doc_set_signal.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc_set_signal.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 17:01:08 by minyekim          #+#    #+#             */
+/*   Updated: 2024/05/06 17:25:23 by minyekim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	here_doc_parent_sigint(int sig)
+{
+	(void)sig;
+	printf("\n");
+}
+
+void	here_doc_child_sigint(int sig)
+{
+	(void)sig;
+	exit(SIGINT_EXIT_CODE);
+}
