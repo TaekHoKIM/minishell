da0dacbe3e9ca4a350302df9aae66d78c0287c28
minyekim <biie2004@naver.com>
2024-05-08 22:50:51 +0900

    Fix : sigpipe가 발생안되는 부분을 해결 사용하지 않는 fd가 제대로 닫히지 않아서 발생 ex) cat /dev/urandom | head -1 cat /dev/urandom을 담당하는 프로세스의 inpipe가 닫히지 않아서 발생함 사용하지 않는 fd를 모두 닫아줌으로 해결

--- main/child_process.c
+++ main/child_process.c
@@ -10,32 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minishell.h"
-#define STDIN 0
-#define STDOUT 1
+#include "../minishell.h"
 
-			// 자식프로세스라는 함수를 하나 만들어서 거기에서 여러가지
-			// 자식이 해야할 일을 처리
-			// 대표적으로 infile, outfile을 확인해서 열어야 하면 열고
-			// 아니면 파이프로 dup을 떠서 한다.
-			// 그리고 확실하게 모든 fd를 close해줘야한다.
-			// 또 중요한건 cmd가 없을 수도 있다는 점을 생각해야 한다.
-			// ex) < infile | > outfile
-			// wait을 해주는 함수를 하나 만들어서 자식 프로세스의 종료
-			// 코드들을 전부 받아준다. 근데 $?명령어가 있어서 이 부분을
-			// 생각해서 마지막 자식 프로세스의 종료 코드는 기억하고 있어야 한다.
-			// 그리고 자식과 부모의 시그널 핸들링이 다르기 때문에 그 부분을 기억하고
-			// 잘 처리해야한다.
-			
-
-static void	pipe_set(int i_fd, int o_fd)
+void	pipe_set(int i_fd, int o_fd)
 {
-	if (i_fd != NO_CHANGE && dup2(i_fd, STDIN) == FAIL)
+	if (i_fd != NO_CHANGE && dup2(i_fd, STDIN_FILENO) == FAIL)
 	{
 		ft_perror("dup2");
 		exit(EXIT_FAILURE);
 	}
-	if (o_fd != NO_CHANGE && dup2(o_fd, STDOUT) == FAIL)
+	if (o_fd != NO_CHANGE && dup2(o_fd, STDOUT_FILENO) == FAIL)
 	{
 		ft_perror("dup2");
 		exit(EXIT_FAILURE);
@@ -50,59 +34,6 @@ static void	pipe_set(int i_fd, int o_fd)
 		ft_perror("close");
 		exit(EXIT_FAILURE);
 	}
-}
-
-// NO_CHANGE, NO_CHANGE
-
-// NO_CHANGE, pipe
-// NO_CHANGE, file
-
-// pipe, NO_CHANGE
-
-// file, NO_CHANGE
-
-// pipe pipe
-
-// pipe file
-
-// file pipe
-
-// file file
-
-
-static void child_fd_set(t_info *info, int i)
-{
-    // 기본적으로 cmd 1개만 있을때 예외 처리를 안해줬음
-    if (i == 0 && info->i_fd == STDIN && info->o_fd == STDOUT && i == info->pipe_cnt)
-        pipe_set(NO_CHANGE, NO_CHANGE);
-    // 여기 부분 추가 된거 위에것
-    else if (i == 0 && info->i_fd == STDIN && info->o_fd == STDOUT)
-        pipe_set(NO_CHANGE, info->pipefd[i][1]);
-    else if (i == 0 && info->i_fd == STDIN && info->o_fd != STDOUT)
-        pipe_set(NO_CHANGE, info->o_fd);
-    else if (i == 0 && info->i_fd != STDIN && info->o_fd == STDOUT
-            && i == info->pipe_cnt)
-        pipe_set(info->i_fd, NO_CHANGE);
-    else if (i == 0 && info->i_fd != STDIN && info->o_fd != STDOUT)
-        pipe_set(info->i_fd, info->o_fd);
-    else if (i == 0 && i == info->pipe_cnt)
-        pipe_set(NO_CHANGE, NO_CHANGE);
-    else if (i == info->pipe_cnt && info->i_fd == STDIN && info->o_fd == STDOUT)
-        pipe_set(info->pipefd[i - 1][0], NO_CHANGE);
-    else if (i == info->pipe_cnt && info->i_fd != STDIN && info->o_fd != STDOUT)
-        pipe_set(info->i_fd, info->o_fd);
-    else if (i == info->pipe_cnt && info->i_fd != STDIN && info->o_fd == STDOUT)
-        pipe_set(info->i_fd, NO_CHANGE);
-    else if (i == info->pipe_cnt && info->i_fd == STDIN && info->o_fd != STDOUT)
-        pipe_set(info->pipefd[i - 1][0], info->o_fd);
-    else if (info->i_fd == STDIN && info->o_fd == STDOUT)
-        pipe_set(info->pipefd[i - 1][0], info->pipefd[i][1]);
-    else if (info->i_fd == STDIN && info->o_fd != STDOUT)
-        pipe_set(info->pipefd[i - 1][0], info->o_fd);
-    else if (info->i_fd != STDIN && info->o_fd == STDOUT)
-        pipe_set(info->i_fd, info->pipefd[i][1]);
-    else if (info->i_fd != STDIN && info->o_fd != STDOUT)
-        pipe_set(info->i_fd, info->o_fd);
 }
 
 static char	*access_check(char **path, char *cmd)
@@ -127,14 +58,22 @@ static char	*access_check(char **path, char *cmd)
 	return (cmd);
 }
 
-void	child_process(t_info *info, int i)
+void	child_process(t_token_list *head, t_envp *envp, t_info *info, int i)
 {
 	char	*cmd_path;
 
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
-	child_fd_set(info, i);
+	if (av_ev_path_file_set(head, envp, info) == FAIL)
+		exit(EXIT_FAILURE);
+	if (i == 0)
+		first_command_fd_set(info, i);
+	else if (i == info->pipe_cnt)
+		last_command_fd_set(info, i);
+	else
+		middle_command_fd_set(info, i);
 	cmd_path = access_check(info->path, info->argv[0]);
+	close_fd();
 	execve(cmd_path, info->argv, info->envp);
 	ft_perror(cmd_path);
 	exit(CMD_NOT_FOUND);
