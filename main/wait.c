df554856ecd728413bf2bbe4961f397077018927
minyekim <biie2004@naver.com>
2024-05-15 02:53:14 +0900

    Add : 시그널로 종료됐을 경우를 추가 메크로를 사용해서 signal로 종료됐는지 확인 후 signal 번호를 추출해서 적절한 종료코드 설정과 인터럽터 메세지를 출력

--- main/wait.c
+++ main/wait.c
@@ -6,7 +6,7 @@
 /*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 19:51:07 by minyekim          #+#    #+#             */
-/*   Updated: 2024/05/12 18:15:56 by minyekim         ###   ########.fr       */
+/*   Updated: 2024/05/15 02:31:41 by minyekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,14 @@ void	child_process_wait(t_info *info)
 	}
 	if (i != 0)
 	{
-		info->exit_code = WEXITSTATUS(status);
+		if (WIFSIGNALED(status) == TRUE)
+			info->exit_code = 128 + WTERMSIG(status);
+		else
+			info->exit_code = WEXITSTATUS(status);
 		info->last_child_pid = info->pid[i - 1];
 	}
+	if (info->exit_code == EXIT_SIGINT)
+		printf("\n");
+	else if (info->exit_code == EXIT_SIGQUIT)
+		printf("Quit: 3\n");
 }
