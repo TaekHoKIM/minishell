e487d22f368adf5e7b044f5a440a04594368537f
minyekim <biie2004@naver.com>
2024-05-15 02:37:51 +0900

    Refactor : norminette

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taekhkim <xorgh456@naver.com>              +#+  +:+       +#+        */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:11:28 by taekhkim          #+#    #+#             */
-/*   Updated: 2024/05/14 16:20:07 by taekhkim         ###   ########.fr       */
+/*   Updated: 2024/05/15 02:29:58 by minyekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	pwd(void)
 	if (getcwd(dir, sizeof(dir)) == NULL)
 		exit(EXIT_FAILURE);
 	printf("built_in pwd:");
-	printf("%s\n",dir);
+	printf("%s\n", dir);
 	exit(EXIT_SUCCESS);
 }
