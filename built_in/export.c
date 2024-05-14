0b29e33130e3e5dcde45aee19340760e1480d3cc
minyekim <biie2004@naver.com>
2024-05-15 02:37:34 +0900

    Refactor : error메세지를 출력하게 변경

--- built_in/export.c
+++ built_in/export.c
@@ -3,39 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taekhkim <xorgh456@naver.com>              +#+  +:+       +#+        */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 17:26:05 by taekhkim          #+#    #+#             */
-/*   Updated: 2024/05/14 19:17:24 by taekhkim         ###   ########.fr       */
+/*   Updated: 2024/05/15 02:36:42 by minyekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	export_check_remove(t_envp *envp, char *arg);
-static void	export_insert(t_envp **envp, char *arg);
-static int	arg_check(char *arg);
-
-int	export(t_envp *envp, char **argv)
+static void	export_insert(t_envp **envp, char *arg)
 {
-	int	i;
-	int	flag;
-	int	exit_code;
+	t_envp	*tmp;
+	t_envp	*new;
+	t_envp	*prev;
 
-	i = 0;
-	exit_code = 0;
-	while (argv[i] != NULL)
+	tmp = (*envp);
+	prev = NULL;
+	while (tmp != NULL)
 	{
-		if (arg_check(argv[i]) == SUCCESS)
+		if (tmp->line == NULL)
 		{
-			if (export_check_remove(envp, argv[i]) == SUCCESS)
-				export_insert(&envp, argv[i]);
+			tmp->line = ft_strdup(arg);
+			return ;
 		}
-		else
-			exit_code = 1;
-		i++;
+		prev = tmp;
+		tmp = tmp->next;
 	}
-	return (exit_code);
+	new = (t_envp *)ft_malloc(sizeof(t_envp), 1);
+	new->line = ft_strdup(arg);
+	new->next = NULL;
+	if (*envp == NULL)
+		*envp = new;
+	else
+		prev->next = new;
 }
 
 static int	export_check_remove(t_envp *envp, char *arg)
@@ -64,57 +65,51 @@ static int	export_check_remove(t_envp *envp, char *arg)
 	return (SUCCESS);
 }
 
-
-static void	export_insert(t_envp **envp, char *arg)
-{
-	t_envp	*tmp;
-	t_envp	*new;
-	t_envp	*pre;
-	char	*input_str;
-
-	tmp = (*envp);
-	pre = NULL;
-	while (tmp != NULL)
-	{
-		if (tmp->line == NULL)
-		{
-			input_str = ft_strdup(arg);
-			tmp->line = input_str;
-			return ;
-		}
-		pre = tmp;
-		tmp = tmp->next;
-	}
-	new = (t_envp *)ft_malloc(sizeof(t_envp), 1);
-	input_str = ft_strdup(arg);
-	new->line = input_str;
-	new->next = NULL;
-	if (pre != NULL)
-		pre->next = new;
-	else if ((*envp) == NULL)
-		(*envp) = new;
-	return ;
-}
-
-static int	arg_check(char *arg)
+static int	arg_check(char *argv)
 {
 	int		len;
-	int		i;
+	int		idx;
 	char	c;
 
 	len = 0;
-	while (arg[len] != '\0' && arg[len] != '=')
+	while (argv[len] != '\0' && argv[len] != '=')
 		len++;
-	c = arg[0];
-	if (!(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_'))
-		return (FAIL);
-	i = 1;
-	while (i < len)
+	idx = 0;
+	while (idx < len)
 	{
-		c = arg[i];
-		if (!(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
-				|| (c == '_') || ('0' <= c && c <= '9')))
+		if (!((idx == 0 && '_' == argv[idx])
+				|| ('A' <= argv[idx] && argv[idx] <= 'Z')
+				|| ('a' <= argv[idx] && argv[idx] <= 'z')
+				|| ('0' <= argv[idx] && argv[idx] <= '9')))
+		{
+			ft_putstr_fd("minishell: export: `", STDERR_FILENO);
+			ft_putstr_fd(argv, STDERR_FILENO);
+			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
 			return (FAIL);
+		}
+		idx++;
+	}
+	return (SUCCESS);
+}
+
+int	export(t_token_list *head, t_envp *envp, t_info *info)
+{
+	int	i;
+	int	flag;
+
+	if (info->pipe_cnt != 0)
+		return (FAIL);
+	i = 0;
+	argv_set(head, info);
+	while (info->argv[i] != NULL)
+	{
+		if (arg_check(info->argv[i]) == SUCCESS)
+		{
+			if (export_check_remove(envp, info->argv[i]) == SUCCESS)
+				export_insert(&envp, info->argv[i]);
+		}
+		else
+			info->exit_code = 1;
 		i++;
 	}
 	return (SUCCESS);
