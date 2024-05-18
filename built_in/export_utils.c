08adfa70a3defcc7fdbd39ab063b046c32de0d2b
minyekim <biie2004@naver.com>
2024-05-18 18:42:12 +0900

    Add : export 인자 없을 때 출력

---
+++ built_in/export_utils.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 21:04:34 by minyekim          #+#    #+#             */
+/*   Updated: 2024/05/17 21:08:15 by minyekim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	export_print(t_envp *envp)
+{
+	while (envp != NULL)
+	{
+		printf("declare -x ");
+		printf("%s\n", envp->line);
+		envp = envp->next;
+	}
+}
