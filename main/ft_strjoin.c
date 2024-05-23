4f5d2e7df10e89792ea3c3454897de9c448c37fa
minyekim <biie2004@naver.com>
2024-05-23 23:21:59 +0900

    Add : null가드를 추가

--- main/ft_strjoin.c
+++ main/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 16:59:51 by minyekim          #+#    #+#             */
-/*   Updated: 2024/05/12 18:00:23 by minyekim         ###   ########.fr       */
+/*   Updated: 2024/05/23 22:02:47 by minyekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len1 = 0;
 	len2 = 0;
 	i = 0;
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
+	if (s1 != NULL)
+		len1 = ft_strlen(s1);
+	if (s2 != NULL)
+		len2 = ft_strlen(s2);
 	tmp = ft_malloc(sizeof(char), (len1 + len2 + 1));
 	ft_strlcpy(tmp, s1, len1 + 1);
 	ft_strlcat(tmp, s2, len1 + len2 + 1);
