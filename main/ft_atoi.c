b63ae88458d8b95400e6df762448f815fe58245a
minyekim <biie2004@naver.com>
2024-05-24 00:01:53 +0900

    Refactor : 매개변수를 주소로 받아 성공여부를 값으로 넘기게 변경 반환으로는 성공여부와 실패여부를 판단하는게 불가능해서 판단을 위해 매개변수를 주소로 받아서 호출한 곳에서 확인할 수 있게 변경함

--- main/ft_atoi.c
+++ main/ft_atoi.c
@@ -6,11 +6,13 @@
 /*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:47:27 by minyekim          #+#    #+#             */
-/*   Updated: 2024/05/15 02:31:09 by minyekim         ###   ########.fr       */
+/*   Updated: 2024/05/23 23:30:29 by minyekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(const char *str)
+#include "../minishell.h"
+
+int	ft_atoi(const char *str, int *check)
 {
 	long long	minus;
 	long long	result;
@@ -30,5 +32,9 @@ int	ft_atoi(const char *str)
 		result = (result * 10) + (*str - '0');
 		str++;
 	}
+	if (*str != '\0')
+		*check = FAIL;
+	else
+		*check = SUCCESS;
 	return ((int)(result * minus));
 }
