6cba2c759eb0d7e2dfe5a6854a2f5d59e7aa9a49
minyekim <biie2004@naver.com>
2024-05-23 23:59:56 +0900

    인자 2개 이상X, +, - 부호 O 인자가 2개 이상일 경우 too many arguments오류를 띄우게 변경 +, -부호는 한 개까지는 들어올 수 있게 변경, 그래서 구조를 조금 변경

--- built_in/exit.c
+++ built_in/exit.c
@@ -6,46 +6,42 @@
 /*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 01:04:42 by minyekim          #+#    #+#             */
-/*   Updated: 2024/05/15 02:29:21 by minyekim         ###   ########.fr       */
+/*   Updated: 2024/05/23 23:50:56 by minyekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	check_exit_argv(char *argv)
+static void	numeric_argument_fail(char *argv)
 {
-	int	idx;
-
-	idx = 0;
-	if (argv == NULL)
-		exit(EXIT_SUCCESS);
-	while (argv[idx] != '\0')
-	{
-		if (!('0' <= argv[idx] && argv[idx] <= '9'))
-		{
-			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-			ft_putstr_fd(argv, STDERR_FILENO);
-			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-			return (FAIL);
-		}
-		idx++;
-	}
-	return (SUCCESS);
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(argv, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+	exit(EXIT_ARGV_NOT_NUM);
 }
 
 int	built_exit(t_token_list *head, t_info *info)
 {
 	int	idx;
-	int	exit_code;
+	int	b_exit_code;
+	int	check;
 
 	idx = 0;
 	if (info->pipe_cnt != 0)
 		return (FAIL);
 	argv_set(head, info);
 	printf("exit\n");
-	if (check_exit_argv(info->argv[1]) == FAIL)
-		exit(EXIT_ARGV_NOT_NUM);
-	exit_code = ft_atoi(info->argv[1]);
-	exit((unsigned char)exit_code);
+	if (info->argv[1] == NULL)
+		exit (EXIT_SUCCESS);
+	b_exit_code = ft_atoi(info->argv[1], &check);
+	if (check == FAIL)
+		numeric_argument_fail(info->argv[1]);
+	else if (info->argv[2] != NULL)
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		g_exit_code = 1;
+		return (SUCCESS);
+	}
+	exit((unsigned char)b_exit_code);
 	return (SUCCESS);
 }
